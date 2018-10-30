5ba62f6868e2ca14560705b8550716e793353461
cjh5414 <cjh5414@gmail.com>
2018-10-30 23:09:07 +0900

    Add Annotation to heeloDI

--- helloDI/src/main/java/kr/ac/hansung/spring/di/PetOwner.java
+++ helloDI/src/main/java/kr/ac/hansung/spring/di/PetOwner.java
@@ -1,6 +1,8 @@
 package kr.ac.hansung.spring.di;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 /**
  * Created by jihun on 2018. 10. 30..
@@ -8,9 +10,16 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class PetOwner {
+
+    @Autowired
+    @Qualifier(value="qf_cat")
     private AnimalType animal;
 
-    public void play() {
+    public PetOwner(AnimalType animal) {
+        this.animal = animal;
+    }
+
+    void play() {
         animal.sound();
     }
 }
